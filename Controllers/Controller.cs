6c4331582be6caafb3023a8f5a182ae5fc4041d4
craciun-st <67415094+craciun-st@users.noreply.github.com>
2024-04-30 08:15:11 +0200

    Project init

---
+++ Controllers/Controller.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Controller : ControllerBase
+{
+    // controller goes here
+}
---
+++ DomainModel/QualityAssessment.cs
@@ -0,0 +1,6 @@
+namespace BasicAPI;
+
+public class QualityAssessment
+{
+    // domain object goes here
+}
